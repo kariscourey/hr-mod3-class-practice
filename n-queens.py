d28b538eb0a2d48d589f3da39b2691db9f3cb732
karis courey <kariscourey@gmail.com>
2022-11-10 10:35:58 -0500

    updated with class example

--- n-queens.py
+++ n-queens.py
@@ -13,60 +13,87 @@
 #     return available_squares
 
 
+# def filter_available_squares(candidate_squares, queen):
+#     available_squares = []
+#     for square in candidate_squares:
+#         # if square[1] != queen[1] and square[0] != queen[0]:
+#         #     if abs(square[1] - queen[1]) != abs(square[0] - queen[0]):
+#         #         available_squares.append(square)
+#         # check columns
+#         if square[1] != queen[1]:
+#             # check rows
+#             if square[0] != queen[0]:
+#                 # check diag
+#                 if abs(square[1] - queen[1]) != abs(square[0] - queen[0]):
+#                     available_squares.append(square)
+#     return available_squares
+
+N = 9
+
+# # available_squares = [
+# #     (0,0),
+# #     (0,1),
+# #     (0,2),
+# #     (0,3),
+# #     (1,0),
+# #     (1,1),
+# #     (1,2),
+# #     (1,3),
+# #     (2,0),
+# #     (2,1),
+# #     (2,2),
+# #     (2,3),
+# #     (3,0),
+# #     (3,1),
+# #     (3,2),
+# #     (3,3),
+# # ]
+
+
+# # n-queens: for each column, do this
+# for i in range(N):
+
+#     queens = []
+#     available_squares = [(r,c) for r in range(N) for c in range(N)][i:]
+
+#     while len(available_squares) > 0:
+#         # get queen from avail squares
+#         queen = available_squares[0]
+#         # add queen to list of queens
+#         queens.append(queen)
+#         # elim squares
+#         available_squares = filter_available_squares(available_squares, queen)
+
+#     if len(queens) == N:
+#         break
+
 def filter_available_squares(candidate_squares, queen):
     available_squares = []
     for square in candidate_squares:
-        # if square[1] != queen[1] and square[0] != queen[0]:
-        #     if abs(square[1] - queen[1]) != abs(square[0] - queen[0]):
-        #         available_squares.append(square)
+        # check rows
+        if square[0] == queen[0]: continue
         # check columns
-        if square[1] != queen[1]:
-            # check rows
-            if square[0] != queen[0]:
-                # check diag
-                if abs(square[1] - queen[1]) != abs(square[0] - queen[0]):
-                    available_squares.append(square)
+        if square[1] == queen[1]: continue
+        # check pos diag
+        if sum(square) == sum(queen): continue
+        # check neg diag
+        if square[0] - square[1] == queen[0] - queen[1]: continue
+        available_squares.append(square)
+
     return available_squares
 
-N = 8
 
-# available_squares = [
-#     (0,0),
-#     (0,1),
-#     (0,2),
-#     (0,3),
-#     (1,0),
-#     (1,1),
-#     (1,2),
-#     (1,3),
-#     (2,0),
-#     (2,1),
-#     (2,2),
-#     (2,3),
-#     (3,0),
-#     (3,1),
-#     (3,2),
-#     (3,3),
-# ]
-
-
-# n-queens: for each column, do this
 for i in range(N):
-
-    queens = []
-    available_squares = [(r,c) for r in range(N) for c in range(N)][i:]
-
+    available_squares = [(r,c) for r in range(N) for c in range(N)]
+    queen = (0, i)
+    queens = [queen]
+    available_squares = filter_available_squares(available_squares, queen)
     while len(available_squares) > 0:
-        # get queen from avail squares
         queen = available_squares[0]
-        # add queen to list of queens
         queens.append(queen)
-        # elim squares
         available_squares = filter_available_squares(available_squares, queen)
-
     if len(queens) == N:
         break
-
 
 
 # verify queens
